a60e1e09ad312d713ac585efb4fa29388305cc5f
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-07-09 08:02:24 +0200

    c

--- detection.py
+++ detection.py
@@ -1,73 +1,104 @@
+import os
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
-import os
-import numpy as np
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-os.system('cls')
-tf.config.run_functions_eagerly(True)
 
-train_dir = 'ai/train'
-validation_dir = 'ai/validation'
+def clear_console():
+    os.system('cls')
 
-batch_size = 32
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest')
+def setup_tensorflow_config():
+    tf.config.run_functions_eagerly(False)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(
+def create_callbacks():
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+    model_checkpoint = ModelCheckpoint('modal_a.h5', monitor='val_loss', save_best_only=True)
+    return [early_stopping, model_checkpoint]
+
+
+def create_data_generators(train_dir, validation_dir, target_size=(100, 80), batch_size=32):
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
         train_dir,
-        target_size=(100, 80),
+        target_size=target_size,
         batch_size=batch_size,
         class_mode='binary')
 
-validation_generator = test_datagen.flow_from_directory(
+    validation_generator = test_datagen.flow_from_directory(
         validation_dir,
-        target_size=(100, 80),
+        target_size=target_size,
         batch_size=batch_size,
         class_mode='binary')
+    
+    return train_generator, validation_generator
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), input_shape=(100, 80, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+def create_model(input_shape=(100, 80, 3)):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), input_shape=input_shape, activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(1, activation='sigmoid')  
-])
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-model.compile(loss='binary_crossentropy',  
-              optimizer='rmsprop',
-              metrics=['accuracy'],
-              run_eagerly=True)
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(1, activation='sigmoid')  
+    ])
 
-model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples // batch_size,
-    epochs=50,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples // batch_size)
+    model.compile(loss='binary_crossentropy',  
+                  optimizer='adam', 
+                  metrics=['accuracy'])
 
-loss, accuracy = model.evaluate(validation_generator)
-print(f'Test loss: {loss}, Test accuracy: {accuracy}')
+    return model
 
-model.save('model_a.h5')
+
+def main():
+    clear_console()
+    setup_tensorflow_config()
+
+    train_dir = 'ai/train'
+    validation_dir = 'ai/validation'
+    batch_size = 32
+
+    callbacks = create_callbacks()
+    train_generator, validation_generator = create_data_generators(train_dir, validation_dir, batch_size=batch_size)
+
+    model = create_model()
+    model.fit(
+        train_generator,
+        steps_per_epoch=train_generator.samples // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=validation_generator.samples // batch_size,
+        callbacks=callbacks  
+    )
+
+    model.load_weights('modal_a.h5')
+
+    loss, accuracy = model.evaluate(validation_generator)
+    print(f'Test loss: {loss}, Test accuracy: {accuracy}')
+
+
+if __name__ == '__main__':
+    main()
